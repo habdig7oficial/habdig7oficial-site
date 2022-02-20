5708c50ff95402fbafae2941bf924d9b969fce71
Mateus Felipe da Silveira Vieira <habdig7@gmail.com>
2022-02-20 00:02:28 -0300

    v0.0.2

---
+++ config/servidor.js
@@ -0,0 +1,15 @@
+/*Importando o módulo do Express framework*/
+const express = require("express")
+
+/*Exeucutando o Express framework */
+const run_express = express
+
+/*definindo porta padrão */
+const porta = 7777
+
+/*permindo acesso aos arquivos de leitura*/
+run_express.use(express.static("./assets/"))
+
+/*exportando variavéis*/
+module.exports = {run_express,porta}
+
---
+++ habdig7.js
@@ -0,0 +1,10 @@
+/*importando configs do servidor*/
+const server_config = require("./config/servidor")
+
+let run_express = server_config.run_express
+
+let porta = server_config.porta
+
+run_express.listen(porta, function name() {
+    console.log("habdig7oficial-site rodando na porta" + porta)
+})
